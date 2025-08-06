b8843b8de6a53e922aa77699a89a5a38b7ec0c45
Navam <navamsharma142@gmail.com>
2025-08-06 12:29:35 +0530

    mongodb env set

---
+++ backend/config/connectDb.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+process.loadEnvFile('../.env');
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+export default connectDb;
