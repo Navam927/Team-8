d41aa849451cbb656aebcba985d27f501248cdcb
nitingoyal05 <nitingoyal9393@gmail.com>
2025-08-06 13:26:49 +0530

    loginSignup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-
+import {Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header'
+import Login from './pages/login'
+import Signup from './pages/signup'
 
 function App() {
   
 
   return (
-    <>
-      <Header/>
-    </>
+  
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+  
   )
 }
 
