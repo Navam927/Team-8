3777c68fba7121f5a32cd3455598790083e67364
nitingoyal05 <nitingoyal9393@gmail.com>
2025-08-06 14:23:41 +0530

    dash

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar';
 
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Dashboard from "./pages/dash";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<h1 className="text-white text-center mt-10 text-3xl">Welcome to Real-Time Collaboration Platform</h1>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
